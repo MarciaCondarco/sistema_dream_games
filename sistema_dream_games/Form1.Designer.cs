b6785c9e25ef475e8f4d99064b8b84f5e477cee4
Marcy_ <marciagisseli21@gmail.com>
2025-02-11 11:24:06 -0300

    Adicionar arquivos de projeto.

---
+++ sistema_dream_games/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace sistema_dream_games
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.textBoxDesenvolvedor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.maskedTextBoxValor = new System.Windows.Forms.MaskedTextBox();
+            this.comboBoxGenero = new System.Windows.Forms.ComboBox();
+            this.richTextBoxDescricao = new System.Windows.Forms.RichTextBox();
+            this.maskedTextBoxTamanho = new System.Windows.Forms.MaskedTextBox();
+            this.comboBoxAvaliacao = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(339, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Jogos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(167, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Titulo";
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(170, 127);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(283, 20);
+            this.textBoxTitulo.TabIndex = 2;
+            // 
+            // textBoxDesenvolvedor
+            // 
+            this.textBoxDesenvolvedor.Location = new System.Drawing.Point(170, 289);
+            this.textBoxDesenvolvedor.Name = "textBoxDesenvolvedor";
+            this.textBoxDesenvolvedor.Size = new System.Drawing.Size(283, 20);
+            this.textBoxDesenvolvedor.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(167, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Desenvolvedor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(168, 367);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Valor";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(566, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Descrição";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(167, 213);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Tamanho";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(167, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Avaliação";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(167, 317);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Genêro";
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(439, 463);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(141, 48);
+            this.buttonSalvar.TabIndex = 15;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // maskedTextBoxValor
+            // 
+            this.maskedTextBoxValor.Location = new System.Drawing.Point(172, 390);
+            this.maskedTextBoxValor.Mask = "$000,00";
+            this.maskedTextBoxValor.Name = "maskedTextBoxValor";
+            this.maskedTextBoxValor.Size = new System.Drawing.Size(283, 20);
+            this.maskedTextBoxValor.TabIndex = 16;
+            // 
+            // comboBoxGenero
+            // 
+            this.comboBoxGenero.FormattingEnabled = true;
+            this.comboBoxGenero.Items.AddRange(new object[] {
+            "aventura",
+            "romance",
+            "ação",
+            "RGP",
+            "Terror",
+            "Tiro"});
+            this.comboBoxGenero.Location = new System.Drawing.Point(170, 343);
+            this.comboBoxGenero.Name = "comboBoxGenero";
+            this.comboBoxGenero.Size = new System.Drawing.Size(282, 21);
+            this.comboBoxGenero.TabIndex = 17;
+            // 
+            // richTextBoxDescricao
+            // 
+            this.richTextBoxDescricao.Location = new System.Drawing.Point(570, 160);
+            this.richTextBoxDescricao.Name = "richTextBoxDescricao";
+            this.richTextBoxDescricao.Size = new System.Drawing.Size(360, 160);
+            this.richTextBoxDescricao.TabIndex = 18;
+            this.richTextBoxDescricao.Text = "";
+            // 
+            // maskedTextBoxTamanho
+            // 
+            this.maskedTextBoxTamanho.Location = new System.Drawing.Point(170, 236);
+            this.maskedTextBoxTamanho.Mask = "00.00GB";
+            this.maskedTextBoxTamanho.Name = "maskedTextBoxTamanho";
+            this.maskedTextBoxTamanho.Size = new System.Drawing.Size(282, 20);
+            this.maskedTextBoxTamanho.TabIndex = 19;
+            // 
+            // comboBoxAvaliacao
+            // 
+            this.comboBoxAvaliacao.FormattingEnabled = true;
+            this.comboBoxAvaliacao.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBoxAvaliacao.Location = new System.Drawing.Point(172, 179);
+            this.comboBoxAvaliacao.Name = "comboBoxAvaliacao";
+            this.comboBoxAvaliacao.Size = new System.Drawing.Size(281, 21);
+            this.comboBoxAvaliacao.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 566);
+            this.Controls.Add(this.comboBoxAvaliacao);
+            this.Controls.Add(this.maskedTextBoxTamanho);
+            this.Controls.Add(this.richTextBoxDescricao);
+            this.Controls.Add(this.comboBoxGenero);
+            this.Controls.Add(this.maskedTextBoxValor);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDesenvolvedor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxTitulo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxTitulo;
+        private System.Windows.Forms.TextBox textBoxDesenvolvedor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxValor;
+        private System.Windows.Forms.ComboBox comboBoxGenero;
+        private System.Windows.Forms.RichTextBox richTextBoxDescricao;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxTamanho;
+        private System.Windows.Forms.ComboBox comboBoxAvaliacao;
+    }
+}
+
---
+++ sistema_dream_games/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace sistema_dream_games
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            string conexaoString = "Server=localhost; Port=3306; Database=db_dream_game; Uid=root; Pwd=;";
+
+            string query = "INSERT INTO tb_games (titulo, avaliacao, tamanho, descricao, valor, desenvolvedor, genero) VALUES " +
+    "(@titulo, @avaliacao, @tamanho, @descricao, @valor, @desenvolvedor, @genero)";
+
+            using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+            {
+                try
+                {
+                    conexao.Open();
+
+                    using (MySqlCommand comando = new MySqlCommand(query, conexao))
+                    {
+                        comando.Parameters.AddWithValue("@titulo", textBoxTitulo.Text);
+                        comando.Parameters.AddWithValue("@avaliacao", comboBoxAvaliacao.Text);
+                        comando.Parameters.AddWithValue("@tamanho", maskedTextBoxTamanho.Text);
+                        comando.Parameters.AddWithValue("@valor", maskedTextBoxValor.Text);
+                        comando.Parameters.AddWithValue("@desenvolvedor", textBoxDesenvolvedor.Text);
+                        comando.Parameters.AddWithValue("@descricao", richTextBoxDescricao.Text);
+                        comando.Parameters.AddWithValue("@genero", comboBoxGenero.Text);
+
+
+                        comando.ExecuteNonQuery();
+                        MessageBox.Show("Dados inseridos com sucesso!");
+
+                        textBoxTitulo.Text = "";
+                        comboBoxAvaliacao.Text = "";
+                        maskedTextBoxTamanho.Text = "";
+                        maskedTextBoxValor.Text = "";
+                        textBoxDesenvolvedor.Text = "";
+                        richTextBoxDescricao.Text = "";
+                        comboBoxGenero.Text = "";
+                        textBoxTitulo.Focus();
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //em caso de erro, exiba mensagem do erro 
+                    MessageBox.Show("Erro: " + ex.Message);
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
